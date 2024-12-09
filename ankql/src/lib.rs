ba73484c7a56eb2ce166681b82b61448f2dc2a7f
Daniel Norman <daniel@danielnorman.net>
2024-12-09 11:23:08 -0800

    Create ankql subcrate

---
+++ ankql/src/lib.rs
@@ -0,0 +1,4 @@
+//! Ankql is a query language for Ankurah in the style of Open Cypher / . The intention of which is to not totally reinvent the wheel, but to provide a
+//! fast-to-implement query language for Ankurah which has only the necessary features to be useful.
+//!
+//! If this pans out, we might someday consider a more complete implementation and ISO/IEC 39075:2024 compliance.
