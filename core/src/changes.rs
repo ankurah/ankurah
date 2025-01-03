aa1279e51c9e563e3830926b18826cf20f68463b
Daniel Norman <daniel@danielnorman.net>
2025-01-03 08:56:41 -0800

    planning notes

--- core/src/changes.rs
+++ core/src/changes.rs
@@ -17,6 +17,30 @@ pub struct RecordChange {
     pub kind: RecordChangeKind,
 }
 
+// I think RecordChange needs to be this:
+// pub enum RecordChange {
+//     /// Initial retrieval of a record upon subscription
+//     Initial{
+//         record: Arc<RecordInner>,
+//         // we don't have updates here because we only have the initial state
+//     },
+//     /// A new record was added OR changed such that it now matches the subscription
+//     Add{
+//         record: Arc<RecordInner>,
+//         updates: Vec<RecordEvent>, // these updates can be relayed by the local node to the remote subscribing node via a NodeRequestBody::CommitEvents message
+//     },
+//     /// A record that previously matched the subscription has changed in a way that has not changed the matching condition
+//     Update{
+//         record: Arc<RecordInner>,
+//         updates: Vec<RecordEvent>, // these updates can be relayed by the local node to the remote subscribing node via a NodeRequestBody::CommitEvents message
+//     },
+//     /// A record that previously matched the subscription has changed in a way that no longer matches the subscription
+//     Remove{
+//         record: Arc<RecordInner>,
+//         updates: Vec<RecordEvent>, // these updates can be relayed by the local node to the remote subscribing node via a NodeRequestBody::CommitEvents message
+//     },
+// }
+
 /// A set of changes to the record set
 #[derive(Debug, Clone)]
 pub struct ChangeSet {
