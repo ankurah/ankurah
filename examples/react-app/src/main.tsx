604071e3039182025f13c8d44bb2a6f868d3fa18
Daniel Norman <daniel@danielnorman.net>
2025-01-06 20:55:33 -0800

    Prettier all the things!

--- examples/react-app/src/main.tsx
+++ examples/react-app/src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { AppStateProvider } from './AppState.tsx'
-import init_bindings from 'example-wasm-bindings';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { AppStateProvider } from "./AppState.tsx";
+import init_bindings from "example-wasm-bindings";
 
 init_bindings().then(async () => {
-  createRoot(document.getElementById('root')!).render(
+  createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <AppStateProvider>
         <App />
       </AppStateProvider>
-    </StrictMode>
-  )
-})
+    </StrictMode>,
+  );
+});
