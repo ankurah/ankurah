5766895f48e50a7e82fe68d3c669ab171b902ee0
Daniel Norman <daniel@danielnorman.net>
2025-04-05 21:20:55 -0700

    instrument feature

--- connectors/websocket-server/src/server.rs
+++ connectors/websocket-server/src/server.rs
@@ -1,7 +1,7 @@
 use ankurah_core::storage::StorageEngine;
 use ankurah_proto as proto;
 use anyhow::Result;
-use axum::extract::{connect_info::ConnectInfo, State};
+use axum::extract::State;
 use axum::{
     extract::ws::{WebSocket, WebSocketUpgrade},
     response::IntoResponse,
@@ -11,12 +11,17 @@ use axum::{
 use axum_extra::{headers, TypedHeader};
 use bincode::deserialize;
 use futures_util::StreamExt;
+use std::net::IpAddr;
 use std::{net::SocketAddr, ops::ControlFlow};
 use tower::ServiceBuilder;
 use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
+#[cfg(feature = "instrument")]
+use tracing::instrument;
 use tracing::{debug, error, info, warn, Level};
 
 use ankurah_core::{node::Node, policy::PolicyAgent};
+
+use crate::client_ip::SmartClientIp;
 
 use super::state::Connection;
 
@@ -61,8 +66,8 @@ where
 
 async fn ws_handler<SE, PA>(
     ws: WebSocketUpgrade,
+    SmartClientIp(client_ip): SmartClientIp,
     user_agent: Option<TypedHeader<headers::UserAgent>>,
-    ConnectInfo(addr): ConnectInfo<SocketAddr>,
     State(node): State<Node<SE, PA>>,
 ) -> impl IntoResponse
 where
@@ -71,33 +76,34 @@ where
 {
     debug!("Websocket server upgrading connection");
     let user_agent = if let Some(TypedHeader(user_agent)) = user_agent { user_agent.to_string() } else { String::from("Unknown browser") };
-    debug!("`{user_agent}` at {addr} connected.");
-    ws.on_upgrade(move |socket| handle_socket(socket, addr, node))
+    debug!("`{user_agent}` at {client_ip} connected.");
+    ws.on_upgrade(move |socket| handle_websocket(socket, client_ip, node))
 }
 
-async fn handle_socket<SE, PA>(socket: WebSocket, who: SocketAddr, node: Node<SE, PA>)
+#[cfg_attr(feature = "instrument", instrument(skip_all, fields(client_ip = %client_ip)))]
+async fn handle_websocket<SE, PA>(socket: WebSocket, client_ip: IpAddr, node: Node<SE, PA>)
 where
     SE: StorageEngine + Send + Sync + 'static,
     PA: PolicyAgent + Send + Sync + 'static,
 {
-    info!("Websocket server connected to {}", who);
+    info!("Websocket server connected to {}", client_ip);
 
     let (sender, mut receiver) = socket.split();
     let mut conn = Connection::Initial(Some(sender));
 
     // Immediately send server presence after connection
     if let Err(e) = conn.send(proto::Message::Presence(proto::Presence { node_id: node.id.clone(), durable: node.durable })).await {
-        debug!("Error sending presence to {who}: {:?}", e);
+        debug!("Error sending presence to {client_ip}: {:?}", e);
         return;
     }
 
     while let Some(msg) = receiver.next().await {
         if let Ok(msg) = msg {
-            if process_message(msg, who, &mut conn, node.clone()).await.is_break() {
+            if process_message(msg, client_ip, &mut conn, node.clone()).await.is_break() {
                 break;
             }
         } else {
-            debug!("client {who} abruptly disconnected");
+            debug!("client {client_ip} abruptly disconnected");
             break;
         }
     }
@@ -108,12 +114,13 @@ where
         node.deregister_peer(peer_sender.recipient_node_id());
     }
 
-    debug!("Websocket context {who} destroyed");
+    debug!("Websocket context {client_ip} destroyed");
 }
 
+#[cfg_attr(feature = "instrument", instrument(skip_all, fields(client_ip = %client_ip)))]
 async fn process_message<SE, PA>(
     msg: axum::extract::ws::Message,
-    who: SocketAddr,
+    client_ip: IpAddr,
     state: &mut Connection,
     node: Node<SE, PA>,
 ) -> ControlFlow<(), ()>
@@ -123,7 +130,7 @@ where
 {
     match msg {
         axum::extract::ws::Message::Binary(d) => {
-            debug!(">>> {} sent {} bytes", who, d.len());
+            debug!(">>> {} sent {} bytes", client_ip, d.len());
 
             if let Ok(message) = deserialize::<proto::Message>(&d) {
                 match message {
@@ -131,7 +138,7 @@ where
                         match state {
                             Connection::Initial(sender) => {
                                 if let Some(sender) = sender.take() {
-                                    debug!("Received client presence from {}", who);
+                                    debug!("Received client presence from {}", client_ip);
 
                                     use super::sender::WebSocketClientSender;
                                     // Register peer sender for this client
@@ -141,41 +148,41 @@ where
                                     *state = Connection::Established(sender);
                                 }
                             }
-                            _ => warn!("Received presence from {} but already have a peer sender - ignoring", who),
+                            _ => warn!("Received presence from {} but already have a peer sender - ignoring", client_ip),
                         }
                     }
                     proto::Message::PeerMessage(msg) => {
                         if let Connection::Established(_) = state {
                             tokio::spawn(async move {
                                 if let Err(e) = node.handle_message(msg).await {
-                                    error!("Error handling message from {}: {:?}", who, e);
+                                    error!("Error handling message from {}: {:?}", client_ip, e);
                                 }
                             });
                         } else {
-                            warn!("Received peer message from {} but not connected as a peer", who);
+                            warn!("Received peer message from {} but not connected as a peer", client_ip);
                         }
                     }
                 }
             } else {
-                error!("Failed to deserialize message from {}", who);
+                error!("Failed to deserialize message from {}", client_ip);
             }
         }
         axum::extract::ws::Message::Text(t) => {
-            debug!(">>> {who} sent str: {t:?}");
+            debug!(">>> {client_ip} sent str: {t:?}");
         }
         axum::extract::ws::Message::Close(c) => {
             if let Some(cf) = c {
-                debug!(">>> {} sent close with code {} and reason `{}`", who, cf.code, cf.reason);
+                debug!(">>> {} sent close with code {} and reason `{}`", client_ip, cf.code, cf.reason);
             } else {
-                debug!(">>> {who} somehow sent close message without CloseFrame");
+                debug!(">>> {client_ip} somehow sent close message without CloseFrame");
             }
             return ControlFlow::Break(());
         }
         axum::extract::ws::Message::Pong(v) => {
-            debug!(">>> {who} sent pong with {v:?}");
+            debug!(">>> {client_ip} sent pong with {v:?}");
         }
         axum::extract::ws::Message::Ping(v) => {
-            debug!(">>> {who} sent ping with {v:?}");
+            debug!(">>> {client_ip} sent ping with {v:?}");
         }
     }
     ControlFlow::Continue(())
