fbfeefc5b0a8f85b856e47c55736fd556a6a06c8
Michael MacFadden <michael@macfadden.org>
2025-04-23 17:13:29 -0500

    Added boolean support for model properties.

--- storage/postgres/src/lib.rs
+++ storage/postgres/src/lib.rs
@@ -238,6 +238,7 @@ impl StorageCollection for PostgresBucket {
                     PGValue::Integer(number) => params.push(number),
                     PGValue::BigInt(number) => params.push(number),
                     PGValue::Bytea(bytes) => params.push(bytes),
+                    PGValue::Boolean(bool) => params.push(bool),
                 },
                 None => params.push(&UntypedNull),
             }
