5661264af3d95a2ef7d481580a6e4426c4ee499a
Daniel Norman <daniel@danielnorman.net>
2025-09-10 09:28:34 -0700

    rename to engine.rs

--- storage/indexeddb-wasm/src/lib.rs
+++ storage/indexeddb-wasm/src/lib.rs
@@ -2,10 +2,10 @@ mod cb_future;
 mod cb_stream;
 mod collection;
 pub mod database;
-mod indexeddb;
+mod engine;
 pub mod indexes;
 mod object;
 mod statics;
 
 pub use collection::{to_idb_cursor_direction, IndexedDBBucket};
-pub use indexeddb::IndexedDBStorageEngine;
+pub use engine::IndexedDBStorageEngine;
