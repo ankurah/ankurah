c8aca2bd76593d7b45eceae74ea5d97e04ae526d
Daniel Norman <daniel@danielnorman.net>
2026-01-26 12:33:53 -0800

    Fix CBStream to remove event listeners on drop
    CBStream was not removing its event listeners when dropped, causing
    "closure invoked recursively or after being dropped" errors when
    IndexedDB fired events after the CBStream was freed.
    
    The fix stores event names in _registrations (like CBFuture already does)
    and removes listeners in the Drop implementation before the closures
    are freed by wasm_bindgen.

--- storage/indexeddb-wasm/src/util/cb_stream.rs
+++ storage/indexeddb-wasm/src/util/cb_stream.rs
@@ -4,22 +4,23 @@ use std::cell::RefCell;
 use std::pin::Pin;
 use std::rc::Rc;
 use std::task::{Context, Poll};
+use tracing::error;
 use wasm_bindgen::prelude::*;
 use web_sys::{Event, EventTarget};
 
-pub fn cb_stream<T: AsRef<EventTarget>>(target: &T, success_event: &str, error_event: &str) -> CBStream {
+pub fn cb_stream<T: AsRef<EventTarget>>(target: &T, success_event: &'static str, error_event: &'static str) -> CBStream {
     CBStream::new(target, success_event, error_event)
 }
 
 pub struct CBStream {
     receiver: mpsc::UnboundedReceiver<Result<JsValue, Event>>,
-    _callbacks: Vec<(Closure<dyn FnMut(Event)>, EventTarget)>,
+    _registrations: Vec<(&'static str, Closure<dyn FnMut(Event)>, EventTarget)>,
 }
 
 impl CBStream {
-    pub fn new<T: AsRef<EventTarget>>(target: &T, success_event: &str, error_event: &str) -> Self {
+    pub fn new<T: AsRef<EventTarget>>(target: &T, success_event: &'static str, error_event: &'static str) -> Self {
         let (sender, receiver) = mpsc::unbounded();
-        let mut callbacks = Vec::new();
+        let mut registrations = Vec::new();
         let target = target.as_ref();
 
         let sender = Rc::new(RefCell::new(Some(sender)));
@@ -38,7 +39,7 @@ impl CBStream {
         let error_callback = Closure::wrap(Box::new({
             let sender = sender;
             move |event: Event| {
-                web_sys::console::error_2(&JsValue::from_str("CB Stream error"), &event);
+                error!("CB Stream error: {}", crate::error::extract_message(event.clone().into()));
                 if let Some(sender) = sender.borrow().as_ref() {
                     let _ = sender.unbounded_send(Err(event));
                 }
@@ -46,13 +47,12 @@ impl CBStream {
         }) as Box<dyn FnMut(_)>);
 
         target.add_event_listener_with_callback(success_event, success_callback.as_ref().unchecked_ref()).unwrap();
-
         target.add_event_listener_with_callback(error_event, error_callback.as_ref().unchecked_ref()).unwrap();
 
-        callbacks.push((success_callback, target.clone()));
-        callbacks.push((error_callback, target.clone()));
+        registrations.push((success_event, success_callback, target.clone()));
+        registrations.push((error_event, error_callback, target.clone()));
 
-        Self { receiver, _callbacks: callbacks }
+        Self { receiver, _registrations: registrations }
     }
 }
 
@@ -60,4 +60,13 @@ impl Stream for CBStream {
     type Item = Result<JsValue, Event>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> { Pin::new(&mut self.receiver).poll_next(cx) }
+}
+
+impl Drop for CBStream {
+    fn drop(&mut self) {
+        // Remove all registered listeners before dropping callbacks
+        for (event_name, callback, target) in &self._registrations {
+            let _ = target.remove_event_listener_with_callback(event_name, callback.as_ref().unchecked_ref());
+        }
+    }
 }
